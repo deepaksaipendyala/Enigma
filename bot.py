9d187f428b51f6945c13aefe3c5d81fa1c4ca535
Riley Joncas <rbjoncas@ncsu.edu>
2024-10-08 17:31:14 -0400

    Fixed an error where cogs were not loading. Cogs must be loaded before having the bot join a voice channel

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from src.songs_cog import Songs
 load_dotenv('.env')
 TOKEN = os.getenv('DISCORD_TOKEN')
 # This can be obtained using ctx.message.author.voice.channel
-VOICE_CHANNEL_ID = 1017135653789646851
+VOICE_CHANNEL_ID = 1293317419279843392
 intents = discord.Intents.all()
 intents.members = True
 client = commands.Bot(command_prefix='/', intents=intents)
@@ -27,10 +27,21 @@ Function that gets executed once the bot is initialized
 
 @client.event
 async def on_ready():
+    print(f'Bot is ready as {client.user}')
+
+    try:
+        print("Loading songs cog")
+        await client.load_extension("src.songs_cog")
+        print("Songs cog loaded successfully")
+    except Exception as e:
+        print(f"Error loading songs cog: {e}")
+
     voice_channel = client.get_channel(VOICE_CHANNEL_ID)
     if client.user not in voice_channel.members:
         await voice_channel.connect()
-    await client.load_extension("src.songs_cog")
+
+    
+
 
 
 """
@@ -42,11 +53,11 @@ Function that is executed once any message is received by the bot
 async def on_message(message):
     if message.author == client.user:
         return
-    options = set()
+    # options = set()
 
-    if message.channel.name == 'general':
-        user_message = str(message.content)
-        await client.process_commands(message)
+    # if message.channel.name == 'general':
+    #     user_message = str(message.content)
+    await client.process_commands(message)
 
 
 """
