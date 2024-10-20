74387b6db242490969d366a67d7f59c5a336c11c
Riley Joncas <rbjoncas@ncsu.edu>
2024-10-20 13:46:41 -0400

    Stared working on adding cosine similarity for an enhanced reccomendation system

--- src/utils.py
+++ src/utils.py
@@ -2,13 +2,16 @@
 This file is responsible for all the helper functions that are used
 """
 from youtubesearchpython import VideosSearch
-from src.get_all import filtered_songs
-"""
-This function seaches the song on youtube and returns the URL
-"""
+from src.get_all import filtered_songs, get_all_songs, get_all_songs_alternate
+import numpy as np
+
 
 
 def searchSong(name_song):
+    """
+    This function seaches the song on youtube and returns the URL
+    """
+
     videosSearch = VideosSearch(name_song, limit=1)
     result = videosSearch.result()
     link = result['result'][0]['link']
@@ -16,12 +19,66 @@ def searchSong(name_song):
 
 
 all_songs = filtered_songs()[["track_name", "artist", "genre"]]
-"""
-This function returns random 25 songs for generating the poll for the user
-"""
+
 
 
 def random_25():
+    """
+    This function returns random 25 songs for generating the poll for the user
+    """
+
     random_songs = (all_songs.sample(
         frac=1).groupby('genre').head(1)).sample(25)
     return random_songs
+
+
+def retrieve_song_attributes(songName, artistName):
+    """
+    This function returns the attributes of the song
+    """
+    try:
+
+        # Get the song attributes from the all_songs dataframe
+        all_songs = get_all_songs().filter(["artist_name","track_name","dating","violence","world/life","night/time","shake the audience","family/gospel","romantic","communication","obscene","music","movement/places","light/visual perceptions","family/spiritual",'like/girls',"sadness","feelings","danceability","loudness","acousticness","instrumentalness","valence","energy"])
+
+        # Get the attributes of the song
+        song = all_songs.loc[(all_songs["track_name"] == songName) & (all_songs["artist_name"] == artistName)]
+
+        song_attributes = song.values.tolist()[0]
+        # Return the attributes
+        return song_attributes[2:]
+
+    except:
+        return [0]*22
+
+
+def retrieve_attributes_alternate(songName, artistName):
+    """
+    This function will retrieve the attributes of the song from the alternate dataset, in the event
+    that the song is not found in the main dataset
+    """
+
+    # Get the songs from the alternate dataset
+    all_songs_alt = get_all_songs_alternate().filter(["track_name", "artist", "bpm", "nrgy", "dnce", "live", "val", "dur", "acous", "spch", "pop"])
+
+    # Get the attributes of the song
+    song = all_songs_alt.loc[(all_songs_alt["track_name"] == songName) & (all_songs_alt["artist"] == artistName)]
+
+    # Return the attributes
+    return song.values.tolist()[0][2:]
+
+
+def cosine_similarity(songName1, artistName1, songName2, artistName2):
+    """
+    This function returns the cosine similarity between two vectors, in this case the attributes of the songs
+    """
+
+    # Get the attributes of the songs
+    song1 = retrieve_song_attributes(songName1, artistName1)
+    song2 = retrieve_song_attributes(songName2, artistName2)
+
+    # Calculate the cosine similarity
+    cos_sim = np.dot(song1, song2) / (np.linalg.norm(song1) * np.linalg.norm(song2))
+
+    return cos_sim
+
--- tests/utils_test.py
+++ tests/utils_test.py
@@ -22,3 +22,54 @@ class Tests(unittest.TestCase):
     def test_random_25(self):
         random_songs = utils.random_25()
         self.assertTrue(len(random_songs) == 25)
+
+
+
+
+class CosSimTests(unittest.TestCase):
+
+    def test_retrieve_song_attributes(self):
+        song_name = "i believe"
+        artist_name = "frankie laine"
+        actual_song_attributes = utils.retrieve_song_attributes(song_name, artist_name)
+        expected_song_attributes = [0.0355371338259024,0.09677674227829695,0.44343517381864045,0.0012836971138939872,0.0012836970540271917,0.02700747737752981,0.0012836971498796242,0.0012836971222831008,0.0012836971144120706,0.11803384116823598,0.001283697092589732,0.2126810671851602,0.05112419901776462,0.0012836970563617238,0.0012836971300268928,0.0012836971751683166,0.33174482833315283,0.64753993282568,0.9548192317462166,1.5283400809716598e-06,0.3250206100577081,0.2632402533492537]
+
+        print(len(actual_song_attributes))
+
+        self.assertTrue(len(actual_song_attributes) == len(expected_song_attributes))
+
+        for i in range(len(actual_song_attributes)):
+            expected_attr = round(expected_song_attributes[i], 10)
+            actual_attr = round(actual_song_attributes[i], 10)
+            self.assertEqual(expected_attr, actual_attr)
+
+    # For testing the cosine_similarity() method. Given two song names, should return the cosine similarity between them.
+    def test_cosine_similarity(self):
+        song_name1 = "cry"
+        artist_name1 = "johnnie ray"
+        song_name2 = "the carioca"
+        artist_name2 = "les paul"
+        actual_cosine_similarity = utils.cosine_similarity(song_name1, artist_name1, song_name2, artist_name2)
+
+        expected_cosine_similarity = 0.75
+        
+        self.assertEqual(expected_cosine_similarity, round(actual_cosine_similarity, 2))
+
+    def test_not_found_song(self):
+        song_name = "song"
+        artist_name = "artist"
+        actual_song_attributes = utils.retrieve_song_attributes(song_name, artist_name)
+        
+        expected_song_attributes = [0]*22
+
+        self.assertEquals(actual_song_attributes, expected_song_attributes)
+
+    def test_retrieve_attributes_alternate(self):
+
+        song_name = "Hey, Soul Sister"
+        artist_name = "Train"
+        expected_song_attributes = [97,89,67,8,80,217,19,4,83]
+
+        actual_song_attributes = utils.retrieve_attributes_alternate(song_name, artist_name)
+
+        self.assertEquals(actual_song_attributes, expected_song_attributes)
