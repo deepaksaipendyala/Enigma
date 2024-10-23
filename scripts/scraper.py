31ef97c4945d4fc277eabe54a253ad4754cbf2a6
batadess <batadess@ncsu.edu>
2024-10-22 20:53:48 -0400

    update requirements.txt

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -3,7 +3,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import time
 
-# Spotify authentication
+# Spotify authentication.
 client_credentials_manager = SpotifyClientCredentials(client_id='87f0fd217127470e8d55171d92e5cd33', client_secret='6094043b04f14d4a98600f76be162838')
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
@@ -24,18 +24,18 @@ songs_df = pd.read_csv('.\\data\\tcc_ceds_music.csv')
 # Create a new column to store the popularity.
 songs_df['popularity'] = None
 
-# Iterate over each song and fetch popularity
+# Iterate over each song and fetch popularity.
 for idx, row in songs_df.iterrows():
     song_name = row['track_name']
     artist_name = row['artist_name']
     popularity = fetch_spotify_popularity(song_name, artist_name)
-    # print(f'Music: {row["track_name"]} Artist: {row["artist_name"]} {popularity}')
+    print(f'Music: {row["track_name"]} Artist: {row["artist_name"]} {popularity}')
     songs_df.at[idx, 'popularity'] = popularity
 
-    # To avoid hitting rate limits, sleep for a bit between requests
+    # To avoid hitting rate limits, sleep for a bit between requests.
     time.sleep(1)
 
-# Save the updated dataset with popularity
+# Save the updated dataset with popularity.
 songs_df.to_csv('tcc_ceds_music_with_popularity.csv', index=False)
 
 print("Popularity data has been added to the dataset.")
